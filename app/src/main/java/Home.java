fe9ade4824e282276087ae947ad31d40de5ea27c
Caleb Moriarty <36317207+CMoriarty95@users.noreply.github.com>
2018-04-02 17:35:42 -0700

    Created Home Class
    Created the class for the home page and added a default and overloaded constructor.

---
+++ app/src/main/java/Home.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class Home {
+    private User user;
+    private List<Group> Groups;
+    private List<Task> Tasks;
+    private List<Chore> Chores;
+    private List<ToDoList> ToDoLists;
+
+    //default constructor; shouldn't be called
+    public Home(){
+        user = null;
+        Groups = null;
+        Tasks = null;
+        Chores = null;
+        ToDoLists = null;
+    }
+
+    //overloaded constructor; takes user as input initializes home's class variables to
+    //the user's stored data
+    public Home(User user){
+
+        this.user = user;
+        this.Groups = this.user.getUsersGroups();
+        this.Tasks = this.user.getUsersTasks();
+        this.Chores = this.user.getUsersChores();
+        this.ToDoLists = this.user.getUsersTDL();
+    }
+
+}
