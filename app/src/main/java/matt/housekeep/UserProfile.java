c04f176da3845b59ced0605bf923b7d879848c9e
Caleb Moriarty <36317207+CMoriarty95@users.noreply.github.com>
2018-04-03 20:09:34 -0700

    Added Data Fields to User Profile Page
    Added fields for all the user data we know we want displayed right now. Need to add more statistics later. Also added a sign out button. The Text Views are in need of alignment

--- app/src/main/java/matt/housekeep/UserProfile.java
+++ app/src/main/java/matt/housekeep/UserProfile.java
@@ -1,11 +1,13 @@
 package matt.housekeep;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class UserProfile extends AppCompatActivity {
 
@@ -17,6 +19,13 @@ public class UserProfile extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button button= (Button) findViewById(R.id.sign_out);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(UserProfile.this,LoginActivity.class));
+            }
+        });
     }
 
 }
