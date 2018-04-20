e9b6b281573a43e431835e5ba9d39a91d88b5cf6
Caleb Moriarty <36317207+CMoriarty95@users.noreply.github.com>
2018-04-20 13:28:25 -0700

    Added Dynamic Buttons User's Groups
    Buttons appear in the horizontal scroll bar at the top of the HomeActivity. Each button has an onClickListener which bundles the user's and group's information and sends it to the GroupActivity to generate a new screen.

---
+++ app/src/main/java/matt/housekeep/GroupActivity.java
@@ -0,0 +1,30 @@
+package matt.housekeep;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GroupActivity extends AppCompatActivity {
+
+    private String groupname;
+    private String username;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle b = getIntent().getExtras();
+        groupname = b.getString("GroupName");
+        username = b.getString("UserName");
+
+        setTitle(groupname);
+    }
+
+}
--- app/src/main/java/matt/housekeep/HomeActivity.java
+++ app/src/main/java/matt/housekeep/HomeActivity.java
@@ -1,22 +1,40 @@
 package matt.housekeep;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 //public class HomeActivity extends AppCompatActivity
 public class HomeActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private ScrollView HomeScroll;
+    private SharedPreferences prefs;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private ArrayList<String> groups;
+    private String username;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +57,99 @@ public class HomeActivity extends AppCompatActivity {
         }
     };
 
+    private void setGroups(ArrayList<String> groups){
+        this.groups = groups;
+    }
+
+    //retrieve basic userdata from database to make buttons for groups and list
+    //user's chores and available tasks
+    private void init(){
+
+        //create/get shared prefs file
+        prefs = this.getSharedPreferences(
+                getString(R.string.shared_prefs_key), Context.MODE_PRIVATE);
+
+        //Log.d("PREFS", getString(R.string.shared_prefs_key));
+
+        //prefs.edit().putString(getString(R.string.saved_username_key), name).apply();
+
+        //String username = prefs.getString(getString(R.string.saved_username_key), "");
+
+        username = "mabrey";
+
+        DatabaseReference myRef = database.getReference("Users/" + username + "/Groups");
+
+        final ArrayList<String> groups = new ArrayList<>();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot newSnap: dataSnapshot.getChildren()){
+
+                    //Store names of groups
+                    groups.add(newSnap.getKey());
+
+                }
+
+                setGroups(groups);
+
+                for(String group: groups){
+                    Log.d("GROUPS", group);
+                }
+
+                makeGroupsButtons();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        //Log.d("FETCHED", username);
+
+
+    }
+
+    private void makeGroupsButtons(){
+
+        LinearLayout ll = findViewById(R.id.GroupLayout);
+
+        for(int i = 0; i < groups.size(); i++){
+
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            Button btn = new Button(this);
+            btn.setId(i);
+            final int id_ = btn.getId();
+            btn.setText(groups.get(id_));
+            ll.addView(btn, params);
+
+            btn = (findViewById(id_));
+            btn.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view) {
+
+                    //Need to dynamically create a new activity with groups[id_]'s info
+
+                    Intent intent = new Intent(HomeActivity.this, GroupActivity.class);
+                    Bundle b = new Bundle();
+                    b.putString("GroupName", groups.get(id_));
+                    b.putString("UserName", username);
+                    intent.putExtras(b);
+                    startActivity(intent);
+
+                    Toast.makeText(view.getContext(), "Button clicked index: " +
+                    id_, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +165,7 @@ public class HomeActivity extends AppCompatActivity {
 
         bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        init();
 
     }
     @Override
