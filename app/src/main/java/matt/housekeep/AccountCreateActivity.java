58e156a36e2675b4872940bd98a6b8ff2fb8be97
Caleb Moriarty <36317207+CMoriarty95@users.noreply.github.com>
2018-04-28 02:04:20 -0700

    Added ability to change name from edit profile

--- app/src/main/java/matt/housekeep/AccountCreateActivity.java
+++ app/src/main/java/matt/housekeep/AccountCreateActivity.java
@@ -1,5 +1,7 @@
 package matt.housekeep;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
